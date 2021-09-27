ec760227f30b0499641b4dbf2f632ddc96664ed2
Christopher Rothert <christopher.rothert@hs-osnabrueck.de>
2021-09-27 17:24:49 +0200

    Fix indentation

--- packages/stacked/example/lib/ui/bottom_nav/favorites/favorites_navigator.dart
+++ packages/stacked/example/lib/ui/bottom_nav/favorites/favorites_navigator.dart
@@ -4,10 +4,13 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class FavoritesNavigator extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-  body: ExtendedNavigator(router: FavoritesNavigatorRouter(), navigatorKey: StackedService.nestedNavigationKey(1)));
+    return Scaffold(
+      body: ExtendedNavigator(
+        router: FavoritesNavigatorRouter(),
+        navigatorKey: StackedService.nestedNavigationKey(1),
+      ),
+    );
   }
 }
